28a73462cf83ca9cf31bf8b5d134d7e36360acde
Maria Stashenko <mahryia@gmail.com>
2025-05-22 18:04:33 +0300

    Added model Author

--- myapp/models.py
+++ myapp/models.py
@@ -2,4 +2,11 @@ from django.db import models
 
 
 # Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    birth_date = models.DateField()
+    death_date = models.DateField(null=True, blank=True)
+
+
 
