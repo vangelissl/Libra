0a8b93c586dd4c1cf04f57d205f6e52ccb8deac9
Maria Stashenko <mahryia@gmail.com>
2025-05-22 18:05:06 +0300

    Added model Book

--- myapp/models.py
+++ myapp/models.py
@@ -9,4 +9,12 @@ class Author(models.Model):
     death_date = models.DateField(null=True, blank=True)
 
 
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    isbn = models.CharField(max_length=100)
+    pages = models.IntegerField()
+    available = models.BooleanField(default=True)
 
+    def __str__(self):
+        return self.title
